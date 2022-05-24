38a9d2af41f0f2834a60bf4039e9c55cec39be51
saurabhfule <68025266+saurabhfule@users.noreply.github.com>
2022-05-24 10:47:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,11 @@
-import './App.css';
-import React,{useState} from 'react';
+import React from 'react';
 
-function App() {
-  
-    const [clear,setClear]=useState(false);
-    const Calculate=(e)=>{
-      e.preventDefault();
-      if(clear) setClear(false);
-      let pa=document.querySelector('#p').value
-      let r=document.querySelector('#r').value
-      let t=document.querySelector('#t').value
-      
-      let interest = (pa*t*r)/100;
-      alert("Simple Interest = "+interest);
-      //document.querySelector('#num').value=interest;
-        
-    } 
-
-    const Clear=(e)=>{
-      e.preventDefault();
-      document.querySelector('form').reset();
-      setClear(true);
-    }
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Simple Interest Calculator
-        </p>
-
-        <form>
-            <label>Principle Amount:</label>
-            <input className="form-control" type="number" id="p"  /> <br></br>
-
-            <label>Rate of Interest:</label>
-            <input className="form-control" type="number" id="r"  /> <br></br>
-
-            <label>Time(Y):</label>
-            <input class="form-control" type="number" id="t"  /> <br></br>
-
-            <button onClick={Calculate}>Calculate</button>
-            <button onClick={Clear}>Clear</button>
-        </form>
-       
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>simon, helloworld!!!</div>
+    );
+  }
 }
 
 export default App;
